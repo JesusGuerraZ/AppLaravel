d4ade16e688ad7a3a44ade4e6c0e24914001ddd0
Jesus David Guerra Zuñiga <jeguezu04@gmail.com>
2021-09-08 11:04:35 -0500

    v2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -23,7 +23,7 @@
         <section class="page-section " id="contact">
             <div class="container">
                 <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Freelance-GP™</h2>
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Freelance-Tech</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom">
                     <div class="divider-custom-line"></div>
