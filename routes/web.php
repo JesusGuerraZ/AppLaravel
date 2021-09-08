0313ca20b41f0c69564510e479474123c4bb0cea
Jesus David Guerra Zuñiga <jeguezu04@gmail.com>
2021-09-07 19:27:25 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('welcome', function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', function () {
     return view('welcome');
 });
 
